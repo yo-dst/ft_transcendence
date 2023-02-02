bcae3a05550a6cd838e243acd18714b9fbbc7e57
nferre <noah.f@orange.fr>
2023-02-02 10:41:23 -0500

    navbar

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,1 +1,31 @@
-<div>Navbar</div>
+<div class="main">
+
+    <button type="button"></button>
+    <h1>ft_transcendence</h1>
+    <img src="default_pp.jpeg" alt="default pp">
+</div>
+    
+<style>
+    .main {
+        background-color:aquamarine;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    h1 {
+        font-size: 100%;
+    }
+
+    img {
+        width: 10vw;
+        border-radius: 50%;
+    }
+
+    button {
+    background-color: transparent;
+    background-image: url(menu.svg);
+    background-size: cover;
+    width: 10vw;
+    border: none;
+    }
+</style>
