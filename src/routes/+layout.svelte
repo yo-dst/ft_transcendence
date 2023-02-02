2dc1a9ea338ddcc6b5cc25bcf3aec4c2285586e3
yo-dst <yoann.danset@gmail.com>
2023-02-02 15:48:47 +0100

    template

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Footer from "$lib/Footer.svelte";
+	import Navbar from "$lib/Navbar.svelte";
+</script>
+
+<div class="main">
+	<Navbar/>
+	<slot></slot>
+	<Footer/>
+</div>
+
+<style>
+	.main {
+		background-color: beige;
+	}
+</style>
