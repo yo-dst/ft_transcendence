8e01799b290fcef3368b7cfe2ed7496a7455fdbb
nferre <noah.f@orange.fr>
2023-02-02 11:50:59 -0500

    added some new things to nav bar

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -6,15 +6,18 @@
 <div class="main">
 
     <button type="button" class="menu" on:click={() => open = true}></button>
+    <img src="pong-icon.svg" alt="icon pong" class="pong-icon"/>
     <h1>ft_transcendence</h1>
     <button type="button" class="pp" ></button>
     
     <Drawer {open} size='50%' placement='left' on:clickAway={() => open=false}>
-        <button class='close' on:click={() => open = false}>close</button>
-        <a href="/">Play</a>
-        <a href="/">Channels</a>
-        <a href="/">Profil</a>
-        <a href="/">Friends</a>
+        <div class="drawer-container">
+            <button class='close' on:click={() => open = false}><img src="xmark.svg" alt="xmark" class="xmark"></button>
+            <a href="/">Play</a>
+            <a href="/">Channels</a>
+            <a href="/">Profil</a>
+            <a href="/">Friends</a>
+        </div>
     </Drawer>
 </div>
     
@@ -26,6 +29,19 @@
         justify-content: space-between;
         margin: 5% 0;
         padding: 0 5%;
+    }
+
+    .pong-icon {
+        width: 20%;
+    }
+
+    .drawer-container {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .xmark {
+        width: 8vw;
     }
 
     h1 {
