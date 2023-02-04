2a5cbc03e07645ccc77db19c088c64303c23c498
yo-dst <yoann.danset@gmail.com>
2023-02-04 14:11:55 +0100

    b4 pulling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,36 @@
+<svelte:head>
+	<script src="https://kit.fontawesome.com/5eedb5e411.js" crossorigin="anonymous"></script>
+</svelte:head>
+
 <script lang="ts">
 	import Footer from "$lib/Footer.svelte";
 	import Navbar from "$lib/Navbar.svelte";
 </script>
 
 <div class="main">
+	<a href="/">Go To Main Page</a>
+	<a href="/profile">Go To Profile Page</a>
 	<Navbar/>
-	<slot></slot>
+	<div class="page">
+		<slot></slot>
+	</div>
 	<Footer/>
+	<i class="fa-solid fa-xmark"></i>
 </div>
 
 <style>
 	.main {
-		background-color: black;
+		background-color: beige;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.page {
+		background-color: antiquewhite;
+		width: 90%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
 	}
 </style>
---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+	const matchHistory = [
+		{
+			opponent: "nferre",
+			win: true,
+			result: {
+				opponnentPoints: 6,
+				myPoints: 7
+			}
+		},
+		{
+			opponent: "mlecherb",
+			win: true,
+			result: {
+				opponnentPoints: 5,
+				myPoints: 8
+			}
+		},
+		{
+			opponent: "ydanset",
+			win: false,
+			result: {
+				opponnentPoints: 14,
+				myPoints: 6
+			}
+		},
+		{
+			opponent: "prout",
+			win: false,
+			result: {
+				opponnentPoints: 4,
+				myPoints: 0
+			}
+		}
+	]
+</script>
+
+<div class="main">
+	Profile Page
+	<div class="presentation">
+		<img src="https://picsum.photos/500/500" alt="profile" class="profile-img"/>
+		<div>Username</div>
+	</div>
+	<div class="match-history">
+		<h3>Match History</h3>
+		{#each matchHistory as match, key}
+			<div class="match">
+				<div>{match.opponent}</div>
+				{#if match.win}
+					<i class="fa-solid fa-check"></i>
+				{:else}
+					<i class="fa-solid fa-xmark"></i>
+				{/if}
+				<div>{match.result.myPoints} / {match.result.opponnentPoints}</div>
+			</div>
+		{/each}
+	</div>
+</div>
+
+<style>
+	.main {
+		background-color: aqua;
+		width: 100%;
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+	}
+
+	.presentation {
+		background-color: bisque;
+		display: flex;
+		align-items: center;
+	}
+
+	.profile-img {
+		width: 50px;
+		height: 50px;
+		border-radius: 50%;
+	}
+
+	.match-history {
+
+	}
+
+	.match {
+		display: flex;
+	}
+</style>
