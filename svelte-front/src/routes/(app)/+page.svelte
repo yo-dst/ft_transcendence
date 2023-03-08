b280bc0b3758180b4b9eeeeafeaef182d52d5dd9
Noah <noah.f@orange.fr>
2023-03-08 11:14:12 +0100

    added nestjs

---
+++ svelte-front/src/routes/(app)/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	let isSearching = false;
+
+	function match() {
+		isSearching = !isSearching;
+	}
+</script>
+
+<section>
+	{#if !isSearching}
+		<button on:click={match}>Find match</button>
+	{:else}
+		<button on:click={match}>Leave</button>
+	{/if}
+	<button>Play vs friends</button>
+</section>
+
+<style>
+</style>
