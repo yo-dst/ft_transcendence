acd6e32bc7fa3d0afd04930f832f64221337297b
Noah <noah.f@orange.fr>
2023-03-29 17:03:27 +0200

    user can reconnect, game will be destroyed after 5 sec dc

--- nest-back/src/game/game.gateway.ts
+++ nest-back/src/game/game.gateway.ts
@@ -1,4 +1,5 @@
 import { ConnectedSocket, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { Socket } from "dgram";
 import { Server } from "socket.io";
 import { gameRooms } from "src/matchmaking/sharedRooms";
 import { CustomSocket } from "./game.customSocket";
@@ -12,6 +13,27 @@ export class GameGateway {
 		client.email = client.handshake.query.email as string;
 
 		client.on('disconnect', () => {
+			const room = gameRooms.find((room) => (room.id === client.roomId));
+			if (room) {
+				if (client.isPlayer) {
+					room.playersMap.set(client.email, true);
+					client.leave(client.roomId);
+					setTimeout(() => {
+						if (room.playersMap.get(client.email)) {
+							this.server.to(client.roomId).emit('decoTimer');
+							setTimeout(() => {
+								if (room.playersMap.get(client.email)) {
+									const index = gameRooms.findIndex((room) => room.id === client.roomId);
+									if (index !== -1) {
+										clearInterval(room.intervalId);
+										gameRooms.splice(index, 1);
+									}
+								}
+							}, 5000)
+						}
+					}, 1000);
+				}
+			}
 		})
 	}
 
@@ -24,6 +46,13 @@ export class GameGateway {
 			client.join(info[0]);
 			client.roomId = info[0];
 			client.isPlayer = room.isPlayer(info[1]);
+			client.email = info[1];
+			if (!room.playersMap.get(client.email)) {
+				room.player[room.index++] = client.email;
+				console.log(room.player[room.index - 1]);
+			}
+			else
+				room.playersMap.set(client.email, false);
 			client.isSpectator = room.isSpecator(info[1]);
 		}
 	}
@@ -31,9 +60,8 @@ export class GameGateway {
 	@SubscribeMessage('ready')
 	checkReady(client: CustomSocket) {
 		const room = gameRooms.find((room) => (room.id === client.roomId));
-		if (client.isPlayer) {
-			room.nbPlayerRdy++;
-			client.playerIndex = room.nbPlayerRdy;
+		if (client.isPlayer && !room.playersMap.get(client.email)) {
+			client.playerIndex = room.nbPlayerRdy++;
 			if (room.nbPlayerRdy == 2) {
 				room.nbPlayerRdy = 0;
 				this.server.to(client.roomId).emit('startTimer');
@@ -55,12 +83,13 @@ export class GameGateway {
 	up(client: CustomSocket, y: number) {
 		const room = gameRooms.find((room) => (room.id === client.roomId));
 		if (client.isPlayer && room.gameInfo != undefined) {
-			if (client.playerIndex === 1) {
+			console.log(client.email, room.player[0])
+			if (client.email === room.player[0]) {
 				//check player index to determine which player should move
 				room.paddle1.dir = 1;
 				this.server.to(client.roomId).emit('playerMove', [y[0] = 1, y[1]]);
 			}
-			else if (client.playerIndex == 2) {
+			else if (client.email == room.player[1]) {
 				room.paddle2.dir = 1;
 				this.server.to(client.roomId).emit('playerMove', [y[0], y[1] = 1]);
 			}
@@ -72,11 +101,11 @@ export class GameGateway {
 		const room = gameRooms.find((room) => (room.id === client.roomId));
 		if (client.isPlayer === true && room.gameInfo != undefined) {
 			//check player index to determine which player should move
-			if (client.playerIndex === 1) {
+			if (client.email === room.player[0]) {
 				room.paddle1.dir = 2;
 				this.server.to(client.roomId).emit('playerMove', [y[0] = 2, y[1]]);
 			}
-			else if (client.playerIndex === 2) {
+			else if (client.email === room.player[1]) {
 				room.paddle2.dir = 2;
 				this.server.to(client.roomId).emit('playerMove', [y[0], y[1] = 2]);
 			}
@@ -87,8 +116,8 @@ export class GameGateway {
 	HandleKeyDown(client: CustomSocket) {
 		const room = gameRooms.find((room) => (room.id === client.roomId));
 		if (client.isPlayer === true && room.gameInfo != undefined) {
-			if (client.playerIndex === 1) room.paddle1.dir = 0;
-			else if (client.playerIndex === 2) room.paddle2.dir = 0;
+			if (client.email === room.player[0]) room.paddle1.dir = 0;
+			else if (client.email === room.player[1]) room.paddle2.dir = 0;
 			this.server.to(client.roomId).emit('Down', [{ x: room.paddle1.x, y: room.paddle1.y, dir: room.paddle1.dir }, { x: room.paddle2.x, y: room.paddle2.y, dir: room.paddle2.dir }]);
 		}
 	}
